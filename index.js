42bd4820273f232408047de10b0edb0e20a9e419
intern <intern@xcellhost.cloud>
2022-02-15 08:46:36 +0530

    added switch tab to js

--- index.js
+++ index.js
@@ -27,13 +27,11 @@ function switchTab(evt, tcid) {
 	tabcontent.forEach((tc)=>{
 	  tc.classList.add("hidden")
 	})
-
 	// Get all elements with class="tablinks" and remove the class "active"
 	var tablinks = document.querySelectorAll(".tablinks");
 	tablinks.forEach((tl)=>{
 	  tl.classList.remove("activeTabLink")
 	})
-
 	// Show the current tab, and add an "active" class to the button that opened the tab
 	document.getElementById(tcid).classList.remove("hidden")
 	evt.classList.add("activeTabLink")
